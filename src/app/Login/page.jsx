d5974d301b1070bb8f78988f9929f41c72ada49a
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-27 20:06:01 +0200

    Login page complete

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -2,11 +2,18 @@
 
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useRouter } from 'next/navigation';
 import { fetchUsers } from '../../redux/features/usersSlice';
+import styles from './login.module.css';
 
 const LoginPage = () => {
   const [users, setUsers] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
   const dispatch = useDispatch();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,14 +28,51 @@ const LoginPage = () => {
     fetchData();
   }, [dispatch]);
 
+  const handleLogin = () => {
+    const foundUser = users.find(
+      (user) => user.email === email && user.password === password
+    );
+  
+    if (foundUser) {
+      router.push('/');
+    } else {
+      setErrorMessage('Invalid email or password');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000); // Hide the error message after 3 seconds
+    }
+  };
+  
+
   return (
-    <div>
-      <h1>Login</h1>
-      {users.map((user, index) => (
-        <p key={index}>
-          {user.email} = {user.password}
-        </p>
-      ))}
+<div className={styles.container}>
+      <h1 className="text-4xl font-bold tracking-widest mt-18">
+        LOGIN PAGE
+      </h1>
+      <form
+        className={styles.form}
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin();
+        }}
+      >
+        <input
+          type="email"
+          className={styles.input} // Added missing className attribute
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className={styles.input} // Added missing className attribute
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errorMessage && <p className={styles.error_message}>{errorMessage}</p>}
+        <button type="submit" className={styles.button}>Login</button> 
+      </form>
     </div>
   );
 };
