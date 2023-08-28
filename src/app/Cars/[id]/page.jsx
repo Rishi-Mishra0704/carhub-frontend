f9eb491e5130b9cf5901c018c3e851dd55591ba2
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-08-29 01:06:37 +0530

    Update car details page

--- src/app/Cars/[id]/page.jsx
+++ src/app/Cars/[id]/page.jsx
@@ -23,12 +23,24 @@ const CarsDetailsPage = () => {
   }
 
   return (
-    <div bg-gray-100 my-24>
+    <div className="bg-gray-100 my-24">
       <div className="bg-white rounded-lg shadow-md mb-4 p-6">
         <h2 className="text-[28px] font-bold text-gray-900 mb-6">{car.name}</h2>
         <div className="border-t border-gray-300 pt-4">
           <p className="text-gray-700 font-semibold mb-4">
             Price: ${car.price}
+          </p>
+          <p className="text-gray-700 font-semibold mb-4">
+            Year: ${car.year}
+          </p>
+          <p className="text-gray-700 font-semibold mb-4">
+            Color: ${car.color}
+          </p>
+          <p className="text-gray-700 font-semibold mb-4">
+            Plate no: ${car.plate_no}
+          </p>
+          <p className="text-gray-700 font-semibold mb-4">
+            Rent Price: ${car.price}
           </p>
         </div>
         <div className="mt-8">
@@ -36,7 +48,7 @@ const CarsDetailsPage = () => {
             href={`/Cars/${car.id}/RentCar`} // Replace ":id" with the actual car ID
             className="inline-block bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
           >
-            View More
+            Rent Car
           </Link>
         </div>
       </div>
